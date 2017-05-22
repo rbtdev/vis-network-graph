fdee69d0ba420baca456c6937e3f93b297587819
Rob Thuleen <rob.thuleen@gmail.com>
2017-05-21 17:27:16 -0700

    Make addNode generic

--- client/src/app/js/graph.js
+++ client/src/app/js/graph.js
@@ -1,4 +1,4 @@
-var ip2long = function (ip) {
+function ip2long(ip) {
     var components;
 
     if (components = ip.match(/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/)) {
@@ -12,7 +12,7 @@ var ip2long = function (ip) {
     } else return -1;
 };
 
-var inSubNet = function (ip, subnet) {
+function inSubNet(ip, subnet) {
     var mask, base_ip, long_ip = ip2long(ip);
     if ((mask = subnet.match(/^(.*?)\/(\d{1,2})$/)) && ((base_ip = ip2long(mask[1])) >= 0)) {
         var freedom = Math.pow(2, 32 - parseInt(mask[2]));
@@ -28,59 +28,43 @@ function construct(hosts, opts) {
 
     var routerId = 0;
     hosts.forEach(function (host, i) {
+
+        // If this is a router, add it as a node, and add it's networks
         if (host.type === 'router') {
-            var image = opts.images ? opts.images['router'] : undefined;
-            var shape = image ? 'image' : undefined;
-            var routerNode = {
-                id: 'router_' + routerId++,
-                label: host.hostname,
-                image: image,
-                shape: shape,
-                details: host
-            }
-            if (nodeMap[routerNode.id]) debugger
-            else nodeMap[routerNode.id] = routerNode;
-            nodes.push(routerNode);
+            var id = 'router_' + routerId++;
+            var node = addNode(id, host);
+
+            // Add each network this router is connected to
             host.networks.forEach(function (network) {
                 // add to networks if not already there
                 if (!networks[network.cidr]) {
                     networks[network.cidr] = network;
-                    var image = opts.images ? opts.images['network'] : undefined;
-                    var shape = image ? 'image' : undefined;
-                    var networkNode = {
-                        id: network.cidr,
-                        label: network.cidr,
-                        image: image,
-                        shape: shape,
-                        details: network
-                    }
-                    if (nodeMap[networkNode.id]) debugger
-                    else nodeMap[networkNode.id] = networkNode;
-                    nodes.push(networkNode);
+                    network.hostname = network.cidr;
+                    network.type = 'network';
+                    var networkNode = addNode(network.cidr, network);
                 }
+
+                // connect the network to the router
                 edges.push({
                     from: network.cidr,
-                    to: routerNode.id,
+                    to: node.id,
                     label: network.address
                 })
             })
         } else if (host.type === 'computer') {
-            var image = opts.images ? opts.images['computer'] : undefined;
-            var shape = image ? 'image' : undefined;
-            var hostNode = {
-                id: host.ip,
-                label: host.ip,
-                image: image,
-                shape: shape,
-                details: host
-            }
-            if (nodeMap[hostNode.id]) debugger
-            else nodeMap[hostNode.id] = hostNode;
-            nodes.push(hostNode);
+
+            // Add a host to the node list and connect it to the appropriate network
+            if (!host.hostname) host.hostname = host.ip;
+            var id = host.ip;
+            var node = addNode(id, host);
+
+            // Find the network that this host belongs to
             Object.keys(networks).forEach(function (networkId) {
                 if (inSubNet(host.ip, networks[networkId].cidr)) {
+
+                    // connect the host to the network
                     edges.push({
-                        from: hostNode.id,
+                        from: node.id,
                         to: networkId
                     })
                 }
@@ -91,8 +75,24 @@ function construct(hosts, opts) {
         nodes: nodes,
         edges: edges
     }
-}
 
+    function addNode(id, entity) {
+        var image = opts.images ? opts.images[entity.type] : undefined;
+        var shape = image ? 'image' : undefined;
+        var node = {
+            id: id,
+            label: entity.hostname,
+            image: image,
+            shape: shape,
+            details: entity
+        }
+        node.title = "<pre>" + JSON.stringify(node, null, 2) + "</pre>"
+        if (nodeMap[node.id]) debugger
+        else nodeMap[node.id] = node;
+        nodes.push(node);
+        return node;
+    }
+}
 
 function Graph(hosts, opts) {
     var graph = construct(hosts, opts);
